1657daf3a51929df1605fa07fecbbce428cdf68d
Susan Buck <susan@susanbuck.net>
2023-09-13 15:15:05 -0400

    First commit

---
+++ app/Http/Controllers/ProductsController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductsController extends Controller
+{
+    public function showProductsByCategory($category = null)
+    {
+        $productsByCategory = [
+            'health' => [
+                'Band-Aids',
+                'Johnson’s Baby Powder',
+                'Tylenol'
+            ],
+            'tech' => [
+                'GoPro Action Camera',
+                'FitBit Fitness Watch',
+                'Nintendo Switch'
+            ],
+            'books' => [
+                'The Martian',
+                'The Great Gatsby',
+                'Joy Luck Club'
+            ]
+        ];
+
+        $categories = array_keys($productsByCategory);
+
+        $products = $productsByCategory[$category] ;
+
+        return view('products')
+            ->with('products', $products)
+            ->with('category', $category)
+            ->with('categories', $categories);
+    }
+}
